127e06679af0c19ed6a05cbffddb00913ee82ad8
Smith Jesko <smithjesko@gmail.com>
2018-06-19 13:07:54 -0500

    Initial commit

---
+++ ecommerce/views.py
@@ -0,0 +1,85 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
+
+
+def home_page(request):
+    context = {
+        "title": "Hello, World!",
+        "content": "Welcome to the home page",
+        
+    }
+
+    if request.user.is_authenticated():
+        context["premium_content"] = "YEAHHHH"
+
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "About",
+        "content": "Welcome to the about page"
+    }
+    return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+
+    context = {
+        "title": "Contact",
+        "content": "Welcome to the contact page",
+        "form": contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    return render(request, "contact/view.html", context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+
+    # print("User logged in")
+    # print(request.user.is_authenticated())
+
+    if form.is_valid():
+        # print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        # print(request.user.is_authenticated())
+
+        if user is not None:
+            # print(request.user.is_authenticated())
+            login(request, user)
+            # context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+
+    return render(request, "auth/register.html", context)
---
+++ products/views.py
@@ -0,0 +1,66 @@
+from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
+
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+class ProductFeaturedDetailView(DetailView):
+    template_name = "products/featured-detail.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+class ProductListView(ListView):
+    template_name = "products/list.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        # instance = get_object_or_404(Product, slug=slug, active=True)
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found...")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Hmm...")
+        return instance
+
+class ProductDetailView(DetailView):
+    # queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product does not exist")
+        return instance
